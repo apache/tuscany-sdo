c480183612bbd1080cf32d62f6b9457c67bc4227
Frank John Budinsky <frankb@apache.org>
2006-05-08 19:49:24 +0000

    Add dummy class as temporary workaround for EMF Eclipse dependency problem
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sdo@405146 13f79535-47bb-0310-9956-ffa450edef68

---
+++ tools/src/main/java/org/eclipse/jdt/core/formatter/CodeFormatter.java
@@ -0,0 +1,8 @@
+/*******************************************************************************
+ * TEMPORARY dummy file to work around EMF generator dependency problem.
+ *  This file will be deleted as soon as the EMF generator is fixed. 
+ *******************************************************************************/
+package org.eclipse.jdt.core.formatter;
+
+public abstract class CodeFormatter {
+}
