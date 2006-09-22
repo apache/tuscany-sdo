5be76bda81b8f5e99c3cab95c899053303eae2c4
Dr. Kelvin James Goodson <kelvingoodson@apache.org>
2006-09-22 14:01:34 +0000

    TUSCANY-653 patch from Yang (I had to hand make the edits as the patch did not want to be applied for some reason) also I added place holders for the README and BUILDING files.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sdo@448940 13f79535-47bb-0310-9956-ffa450edef68

--- tools/src/main/java/org/apache/tuscany/sdo/generate/templates/model/SDOClass.java
+++ tools/src/main/java/org/apache/tuscany/sdo/generate/templates/model/SDOClass.java
@@ -1,3 +1,22 @@
+/**
+ *
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
 package org.apache.tuscany.sdo.generate.templates.model;
 
 import org.eclipse.emf.codegen.util.*;
@@ -1262,23 +1281,6 @@ public class SDOClass
   public String generate(Object argument)
   {
     final StringBuffer stringBuffer = new StringBuffer();
-    
-/**
- *
- *  Copyright 2005 The Apache Software Foundation or its licensors, as applicable.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
 
     GenClass genClass = (GenClass)((Object[])argument)[0]; GenPackage genPackage = genClass.getGenPackage(); GenModel genModel=genPackage.getGenModel();
     boolean isInterface = Boolean.TRUE.equals(((Object[])argument)[1]); boolean isImplementation = Boolean.TRUE.equals(((Object[])argument)[2]);
