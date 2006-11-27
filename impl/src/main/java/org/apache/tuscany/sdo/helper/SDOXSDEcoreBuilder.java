b43c6223ca556a638cf176698b1b15c7d8dc6993
Dr. Kelvin James Goodson <kelvingoodson@apache.org>
2006-11-27 15:31:36 +0000

    permit user xsd file import of commonj.sdo models without overriding built in runtime model
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sdo@479662 13f79535-47bb-0310-9956-ffa450edef68

--- impl/src/main/java/org/apache/tuscany/sdo/helper/SDOXSDEcoreBuilder.java
+++ impl/src/main/java/org/apache/tuscany/sdo/helper/SDOXSDEcoreBuilder.java
@@ -26,6 +26,7 @@ import java.util.List;
 
 import org.apache.tuscany.sdo.SDOExtendedMetaData;
 import org.apache.tuscany.sdo.model.ModelFactory;
+import org.apache.tuscany.sdo.model.impl.ModelFactoryImpl;
 import org.apache.tuscany.sdo.util.SDOUtil;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
@@ -39,7 +40,9 @@ import org.eclipse.xsd.XSDComplexTypeDefinition;
 import org.eclipse.xsd.XSDComponent;
 import org.eclipse.xsd.XSDConcreteComponent;
 import org.eclipse.xsd.XSDFeature;
+import org.eclipse.xsd.XSDImport;
 import org.eclipse.xsd.XSDNamedComponent;
+import org.eclipse.xsd.XSDSchema;
 import org.eclipse.xsd.XSDSimpleTypeDefinition;
 import org.eclipse.xsd.XSDTypeDefinition;
 import org.eclipse.xsd.ecore.XSDEcoreBuilder;
@@ -55,6 +58,7 @@ import org.w3c.dom.Element;
  */
 public class SDOXSDEcoreBuilder extends XSDEcoreBuilder
 {
+  
   public SDOXSDEcoreBuilder(ExtendedMetaData extendedMetaData)
   {
     super(extendedMetaData);
@@ -77,6 +81,8 @@ public class SDOXSDEcoreBuilder extends XSDEcoreBuilder
         getBuiltInEClassifier(
           xsdTypeDefinition.getURI(), 
           xsdTypeDefinition.getName());
+    } else if (ModelFactoryImpl.NAMESPACE_URI.equals(xsdTypeDefinition.getTargetNamespace())) {
+      eClassifier = ((ModelFactoryImpl)ModelFactory.INSTANCE).getEClassifier(xsdTypeDefinition.getName());
     } else {
       eClassifier = super.getEClassifier(xsdTypeDefinition);
     }
@@ -455,4 +461,26 @@ public class SDOXSDEcoreBuilder extends XSDEcoreBuilder
     return qualifiedPackageName.toString().toLowerCase(); //make sure it's lower case .. we can't work with Axis if not.
   }
 
+
+  public void generate(XSDSchema xsdSchema) {
+    // permits schemas to be valid by having an import of sdoModel.xsd without
+    // that import causing masking of the singleton instance of the generated model
+    if(!ModelFactoryImpl.NAMESPACE_URI.equals(xsdSchema.getTargetNamespace())) {
+      List contents = xsdSchema.getContents();
+      List commonjImports = new ArrayList();
+      for (Iterator i = contents.iterator(); i.hasNext(); )
+      {
+        Object content = i.next();
+        if (content instanceof XSDImport)
+        {
+          XSDImport xsdImport = (XSDImport)content;
+          if (ModelFactoryImpl.NAMESPACE_URI.equals(xsdImport.getNamespace())) {
+            commonjImports.add(xsdImport);
+          }
+        }
+      }
+      contents.removeAll(commonjImports);
+      super.generate(xsdSchema);
+    }
+  }
 }
