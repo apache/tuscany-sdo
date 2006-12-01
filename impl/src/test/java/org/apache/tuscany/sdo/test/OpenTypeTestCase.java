05a45980a906993f822b8b1005f8902d5d506625
Dr. Kelvin James Goodson <kelvingoodson@apache.org>
2006-12-01 14:11:12 +0000

    Added a disabled test case for TUSCANY-396 -- the issue still exists
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sdo@481249 13f79535-47bb-0310-9956-ffa450edef68

--- impl/src/test/java/org/apache/tuscany/sdo/test/OpenTypeTestCase.java
+++ impl/src/test/java/org/apache/tuscany/sdo/test/OpenTypeTestCase.java
@@ -28,10 +28,13 @@ import java.math.BigDecimal;
 import java.net.URL;
 import java.util.Iterator;
 
+import org.apache.tuscany.sdo.util.SDOUtil;
+
 import commonj.sdo.DataObject;
 import commonj.sdo.Property;
 import commonj.sdo.Type;
 import commonj.sdo.helper.DataFactory;
+import commonj.sdo.helper.HelperContext;
 import commonj.sdo.helper.TypeHelper;
 import commonj.sdo.helper.XMLHelper;
 import commonj.sdo.helper.XSDHelper;
@@ -39,6 +42,11 @@ import junit.framework.TestCase;
 
 
 public class OpenTypeTestCase extends TestCase {
+    private HelperContext hc;
+    private TypeHelper th;
+    private XSDHelper xsdh;
+    private XMLHelper xmlh;
+    private DataFactory df;
     private final String TEST_MODEL = "/open.xsd";
     private final String TEST_NAMESPACE = "http://www.example.com/open";
     private final String TEST_DATA = "/open.xml";
@@ -47,8 +55,8 @@ public class OpenTypeTestCase extends TestCase {
      * Open content SDO 2 test.
      */
     public void testOpenType() throws IOException {
-        Type quoteType = TypeHelper.INSTANCE.getType(TEST_NAMESPACE, "OpenQuote");
-        DataObject quote = DataFactory.INSTANCE.create(quoteType);
+        Type quoteType = th.getType(TEST_NAMESPACE, "OpenQuote");
+        DataObject quote = df.create(quoteType);
 
         quote.setString("symbol", "s1");
 
@@ -56,11 +64,11 @@ public class OpenTypeTestCase extends TestCase {
             Property property = (Property) iter.next();
         }
 
-        Property companyProperty = XSDHelper.INSTANCE.getGlobalProperty(TEST_NAMESPACE, "company", true);
+        Property companyProperty = xsdh.getGlobalProperty(TEST_NAMESPACE, "company", true);
         DataObject company = quote.createDataObject(companyProperty);
         company.setString("name", "FlyByNightTechnology");
 
-        Property priceProperty = XSDHelper.INSTANCE.getGlobalProperty(TEST_NAMESPACE, "price", true);
+        Property priceProperty = xsdh.getGlobalProperty(TEST_NAMESPACE, "price", true);
         quote.getList(priceProperty).add(new BigDecimal("1000.0"));
 
         for (Iterator iter = quote.getInstanceProperties().iterator(); iter.hasNext();) {
@@ -68,10 +76,38 @@ public class OpenTypeTestCase extends TestCase {
         }
 
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        XMLHelper.INSTANCE.save(quote, TEST_NAMESPACE, "openStockQuote", baos);
+        xmlh.save(quote, TEST_NAMESPACE, "openStockQuote", baos);
         
         assertTrue(TestUtil.equalXmlFiles(new ByteArrayInputStream(baos.toByteArray()), getClass().getResource(TEST_DATA)));
     }
+    
+    // TODO introduce this test once the EMF issue behind TUSCANY-396 has been fixed
+    public void dont_testOpenType_Tuscany_396() throws IOException {
+      Type quoteType = th.getType(TEST_NAMESPACE, "OpenQuote");
+      DataObject quote = df.create(quoteType);
+
+      quote.setString("symbol", "s1");
+
+      for (Iterator iter = quote.getInstanceProperties().iterator(); iter.hasNext();) {
+          Property property = (Property) iter.next();
+      }
+
+      Property companyProperty = xsdh.getGlobalProperty(TEST_NAMESPACE, "company", true);
+      DataObject company = quote.createDataObject(companyProperty);
+      company.setString("name", "FlyByNightTechnology");
+
+      Property priceProperty = xsdh.getGlobalProperty(TEST_NAMESPACE, "price", true);
+      quote.set(priceProperty, new BigDecimal("1000.0"));
+
+      for (Iterator iter = quote.getInstanceProperties().iterator(); iter.hasNext();) {
+          Property property = (Property) iter.next();
+      }
+
+      ByteArrayOutputStream baos = new ByteArrayOutputStream();
+      xmlh.save(quote, TEST_NAMESPACE, "openStockQuote", baos);
+      
+      assertTrue(TestUtil.equalXmlFiles(new ByteArrayInputStream(baos.toByteArray()), getClass().getResource(TEST_DATA)));
+  }
 
     protected void setUp() throws Exception {
         super.setUp();
@@ -79,7 +115,12 @@ public class OpenTypeTestCase extends TestCase {
         // Populate the meta data for the test (Stock Quote) model
         URL url = getClass().getResource(TEST_MODEL);
         InputStream inputStream = url.openStream();
-        XSDHelper.INSTANCE.define(inputStream, url.toString());
+        hc = SDOUtil.createHelperContext();
+        th = hc.getTypeHelper();
+        xsdh = hc.getXSDHelper();
+        df = hc.getDataFactory();
+        xmlh = hc.getXMLHelper();
+        hc.getXSDHelper().define(inputStream, url.toString());
         inputStream.close();
     }
 }
