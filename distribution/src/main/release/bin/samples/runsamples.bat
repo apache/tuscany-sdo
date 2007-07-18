85777cfc41ed4a974e84589a1a1cb2a0d08297d1
Dr. Kelvin James Goodson <kelvingoodson@apache.org>
2007-07-18 11:43:45 +0000

    add README to samples and update organisation of release/bin files
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/branches/sdo-1.0-incubating@557234 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/bin/samples/runsamples.bat
+++ distribution/src/main/release/bin/samples/runsamples.bat
@@ -14,8 +14,13 @@ REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
 REM KIND, either express or implied.  See the License for the
 REM specific language governing permissions and limitations
 REM under the License.
-set BINARY_BASE=edit this variable to point to the root folder of your binary distribution
+@echo off
+set BINARY_BASE=..
 set RELEASE=${sdo.version}
+echo SDO Sample Programs.  Running with BINARY_BASE set to %BINARY_BASE%
+echo If this script fails with ClassDefNotFound errors you probably need to
+echo edit the BINARY_BASE variable in the script to point to the location
+echo where you unpacked the Tuscany SDO binary distribution
 set LIB=%BINARY_BASE%\lib
 set CLASSPATH=%LIB%\sample-sdo-%RELEASE%.jar;%LIB%\tuscany-sdo-api-r2.1-%RELEASE%.jar;%LIB%\tuscany-sdo-lib-%RELEASE%.jar;%LIB%\tuscany-sdo-impl-%RELEASE%.jar;%LIB%\tuscany-sdo-tools-%RELEASE%.jar;%LIB%\codegen-ecore-2.2.3.jar;%LIB%\codegen-2.2.3.jar;%LIB%\ecore-2.2.3.jar;%LIB%\ecore-change-2.2.3.jar;%LIB%\ecore-xmi-2.2.3.jar;%LIB%\common-2.2.3.jar;%LIB%\xsd-2.2.3.jar;%LIB%\stax-api-1.0.1.jar
 java org.apache.tuscany.samples.sdo.ExecuteSamples
