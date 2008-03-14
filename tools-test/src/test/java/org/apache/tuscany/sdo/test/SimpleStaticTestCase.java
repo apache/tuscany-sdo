7e3b405521957b4fb7589a47866a9a71ad2d3cfd
Dr. Kelvin James Goodson <kelvingoodson@apache.org>
2008-03-14 10:27:01 +0000

    added test for TUSCANY-2080
    git-svn-id: https://svn.apache.org/repos/asf/incubator/tuscany/java/sdo@637039 13f79535-47bb-0310-9956-ffa450edef68

--- tools-test/src/test/java/org/apache/tuscany/sdo/test/SimpleStaticTestCase.java
+++ tools-test/src/test/java/org/apache/tuscany/sdo/test/SimpleStaticTestCase.java
@@ -19,7 +19,12 @@
  */
 package org.apache.tuscany.sdo.test;
 
+import java.io.InputStream;
 import java.math.BigDecimal;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.dom.DOMSource;
 
 import junit.framework.TestCase;
 
@@ -27,7 +32,11 @@ import org.apache.tuscany.sdo.api.SDOUtil;
 
 import com.example.simple.Quote;
 import com.example.simple.SimpleFactory;
+
+import commonj.sdo.DataObject;
 import commonj.sdo.helper.HelperContext;
+import commonj.sdo.helper.XMLDocument;
+import commonj.sdo.impl.HelperProvider;
 
 
 public class SimpleStaticTestCase extends TestCase
@@ -41,10 +50,6 @@ public class SimpleStaticTestCase extends TestCase
   {
     try
     {
-      SimpleFactory.INSTANCE.register(scope);
-      // System.out.println(SimpleFactoryImpl.PATTERN_VERSION);
-      
-      //Quote quote = (Quote)DataFactory.INSTANCE.create(Quote.class);
       Quote quote = SimpleFactory.INSTANCE.createQuote();
       
       quote.setSymbol("fbnt");
@@ -68,9 +73,32 @@ public class SimpleStaticTestCase extends TestCase
       e.printStackTrace();
     }
   }
+  
+  public void testSimpleStaticViaDomSource() throws Exception {
+    // created for TUSCANY-2080
+
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    factory.setNamespaceAware(true);
+
+    DocumentBuilder builder = factory.newDocumentBuilder();
+
+    java.net.URL url = getClass().getResource("/stockquote.xml");
+    InputStream inputStream = url.openStream();
+    org.w3c.dom.Document dom = builder.parse(inputStream);
+
+    // Now load from dom to object
+    DOMSource source = new DOMSource(dom);
+
+    XMLDocument xml = scope.getXMLHelper().load(source, null, null);
+    assertTrue("Root element is not an instance of the generated class", xml
+        .getRootObject() instanceof Quote);
+
+  }
 
   protected void setUp() throws Exception {
     scope = SDOUtil.createHelperContext();
+    SimpleFactory.INSTANCE.register(scope);
+    
     super.setUp();
   }
   
